e4a78da0afca519cbf5ab6ec1669007a82e1b3e2
Mike Dougherty <mikedough213@gmail.com>
2021-12-02 15:50:36 -0600

    Import SQL scripts

---
+++ prc/sql/search.sql
@@ -0,0 +1,1 @@
+SELECT * FROM trs WHERE descr LIKE ?; SELECT ROUND(SUM(debit), 2), ROUND(SUM(credit), 2) FROM trs WHERE descr LIKE ?;
---
+++ prc/sql/select.sql
@@ -0,0 +1,2 @@
+SELECT * FROM trs;
+SELECT ROUND(SUM(debit), 2), ROUND(SUM(credit), 2) FROM trs;
